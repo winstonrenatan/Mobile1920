0e72e46690f1221c331e2b708b5f5af3ec1839ee
Winston Renatan <winstonrenatan@gmail.com>
2019-10-16 17:38:42 +0700

    Initial commit

---
+++ Meeting06-FragmentCycle/app/src/main/java/com/github/winstonrenatan/brodoraexplore/HowToPlay.kt
@@ -0,0 +1,43 @@
+package com.github.winstonrenatan.brodoraexplore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.github.winstonrenatan.brodoraexplore.databinding.FragmentHowtoplayBinding
+import timber.log.Timber
+
+class HowToPlay : Fragment() {
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentHowtoplayBinding>(inflater,
+            R.layout.fragment_howtoplay, container, false)
+
+        return binding.root
+    }
+}
---
+++ Meeting06-FragmentCycle/app/src/main/java/com/github/winstonrenatan/brodoraexplore/MainMenu.kt
@@ -0,0 +1,47 @@
+package com.github.winstonrenatan.brodoraexplore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.github.winstonrenatan.brodoraexplore.databinding.FragmentMainmenuBinding
+import timber.log.Timber
+
+class MainMenu : Fragment() {
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy called")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentMainmenuBinding>(inflater,
+            R.layout.fragment_mainmenu, container, false)
+
+        // Listeners ...
+        binding.startButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainMenu_to_gameFragment)
+        }
+
+        return binding.root
+    }
+}
